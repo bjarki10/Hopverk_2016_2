e3437fb254bcaaf4c9a7dcb8e040bbd3c1cbb711
bjarki10 <bjarki1234@gmail.com>
2016-11-28 11:13:24 +0000

    qq

---
+++ hopverk/hopverk/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace hopverk
+{
+    public partial class Form1 : Form
+    {
+        List<int> listi2 = new List<int>();
+
+        Random rnd = new Random();
+
+        List<int> listi = new List<int>();
+
+        List<string> linur = new List<string>();
+
+        string[] arr = new string[4];
+        
+        string karfa;
+
+        Gagnagrunnur gagnagrunnur = new Gagnagrunnur();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                gagnagrunnur.TengingVidGagnagrunn();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            li_korfa.Columns.Add("Nafn", 200);
+            li_korfa.Columns.Add("Voruflokkur", 120);
+            li_korfa.Columns.Add("Verd", 60);
+            li_korfa.Columns.Add("ALager", 60);
+
+            li_lager.Clear();
+            li_lager.Columns.Add("Nafn", 200);
+            li_lager.Columns.Add("Voruflokkur", 120);
+            li_lager.Columns.Add("Verd", 60);
+            li_lager.Columns.Add("ALager", 60);
+
+            ListViewItem itm;
+
+            linur = gagnagrunnur.LesautSQLToflu();
+
+            try
+            {
+
+                foreach (string lin in linur)
+                {
+                    string[] linaUrLista = lin.Split(':');
+                    string Nafn = linaUrLista[0];
+                    string Voruflokkur = linaUrLista[1];
+                    string Verd = linaUrLista[2];
+                    string ALager = linaUrLista[3];
+
+                    arr[0] = Nafn;
+                    arr[1] = Voruflokkur;
+                    arr[2] = Verd;
+                    arr[3] = ALager;
+
+
+                    itm = new ListViewItem(arr);
+                    li_lager.Items.Add(itm);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            li_lager.Clear();
+            li_lager.Columns.Add("Nafn", 200);
+            li_lager.Columns.Add("Voruflokkur", 120);
+            li_lager.Columns.Add("Verd", 60);
+            li_lager.Columns.Add("ALager", 60);
+
+            ListViewItem itm;
+
+            linur = gagnagrunnur.LesautSQLToflu();
+
+            try
+            {
+
+                foreach (string lin in linur)
+                {
+                    string[] linaUrLista = lin.Split(':');
+                    string Nafn = linaUrLista[0];
+                    string Voruflokkur = linaUrLista[1];
+                    string Verd = linaUrLista[2];
+                    string ALager = linaUrLista[3];
+                    if (linaUrLista[0].ToLower().StartsWith(textBox1.Text))
+                    {
+                        arr[0] = Nafn;
+                        arr[1] = Voruflokkur;
+                        arr[2] = Verd;
+                        arr[3] = ALager;
+
+
+                        itm = new ListViewItem(arr);
+                        li_lager.Items.Add(itm);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bt_korfu_Click(object sender, EventArgs e)
+        {
+            
+
+            ListViewItem itm;
+            string[] linaUrLista = karfa.Split(':');
+            string Nafn = linaUrLista[0];
+            string Voruflokkur = linaUrLista[1];
+            string Verd = linaUrLista[2];
+            string Magn = tb_magn.Text;
+
+            if (Magn.Any(x => !char.IsLetter(x)) && Convert.ToInt32(Magn) >= 1)
+            {
+                arr[0] = Nafn;
+                arr[1] = Voruflokkur;
+                arr[2] = Verd;
+                arr[3] = Magn;
+
+
+                itm = new ListViewItem(arr);
+                li_korfa.Items.Add(itm);
+            }
+            else
+            {
+                MessageBox.Show("Setu inn Magn hærri en 0");
+            }
+                
+                
+            
+        }
+        private void li_lager_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (li_lager.SelectedIndices.Count <= 0)
+            {
+                return;
+            }
+            int intSelectIndex = li_lager.SelectedIndices[0];
+            if (intSelectIndex >= 0)
+            {
+                karfa = li_lager.SelectedItems[0].SubItems[0].Text + ":" + li_lager.SelectedItems[0].SubItems[1].Text + ":" + li_lager.SelectedItems[0].SubItems[2].Text;
+            }
+        }
+
+        private void li_korfa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void bt_eda_Click(object sender, EventArgs e)
+        {
+            for (int i = li_korfa.Items.Count - 1; i >= 0; i--)
+            {
+                if (li_korfa.Items[i].Selected)
+                {
+                    li_korfa.Items[i].Remove();
+                }
+            }
+        }
+
+        private void bt_Check_out_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
---
+++ hopverk/hopverk/Gagnagrunnur.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace hopverk
+{
+    class Gagnagrunnur
+    {
+       
+    
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        string tengistrengur;
+        string fyrirspurn;
+
+        MySqlConnection sqltenging;
+        MySqlCommand nySQLskipun;
+        MySqlDataReader sqllesari = null;
+
+        public void TengingVidGagnagrunn()
+        {
+            server = "10.200.10.24";
+            database = "2201992909_hopverk_2016";
+            uid = "2201992909";
+            password = "mypassword";
+
+            tengistrengur = "server=" + server + ";userid=" + uid + ";password=" + password + ";database=" + database;
+            sqltenging = new MySqlConnection(tengistrengur);
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                sqltenging.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                sqltenging.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public List<string> LesautSQLToflu()
+        {
+            List<string> Faerslur = new List<string>();
+            string lina = null;
+            if (OpenConnection() == true)
+            {
+                fyrirspurn = "SELECT Nafn,Voruflokkur,Verd,ALager FROM vorur";
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < sqllesari.FieldCount; i++)
+                    {
+                        lina += (sqllesari.GetValue(i).ToString()) + ":";
+                    }
+                    Faerslur.Add(lina);
+                    lina = null;
+                }
+                CloseConnection();
+                return Faerslur;
+            }
+            return Faerslur;
+        }
+        public void addtotable(string nafn, string skuld)
+        {
+            if (OpenConnection() == true) 
+            {
+                fyrirspurn = "INSERT INTO skuld (nafn, Skuld) VALUES ('" + nafn + "','" + skuld + "')";
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                nySQLskipun.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+        public void removefromtable(string nafn, string skuld)
+        {
+            if (OpenConnection() == true)
+            {
+                fyrirspurn = "DELETE FROM skuld WHERE nafn='" + nafn + "' AND Skuld ='" + skuld + "'";
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                nySQLskipun.ExecuteNonQuery();
+                CloseConnection();
+                }
+            }
+
+
+        
+    }
+}
+
